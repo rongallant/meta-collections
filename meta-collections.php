21b3d4037f3599d3b692968023f517aff94e0fc9
bastiaaaan <bastiaaaan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-08 19:47:30 +0000

    version 1.0.2
    git-svn-id: https://plugins.svn.wordpress.org/meta-collections/tags/1.0.2@596382 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- meta-collections.php
+++ meta-collections.php
@@ -1,19 +1,18 @@
 <?php
 /*
-* @Plugin Name: Meta Collections
-* @Plugin URI: http://wordpress.org/extend/plugins/meta-collections
-* @author Bastiaan Blaauw <statuur@gmail.com>
+* Plugin Name: Meta Collections
+* Plugin URI: http://wordpress.org/extend/plugins/meta-collections
 * Author: Bastiaan Blaauw <statuur@gmail.com>	 
 * Version: 1.0.2
-* @version: 1.0.2
 * @see http://metacollections.statuur.nl/ 
 * @category Wordpress Plugin
 * @package  Collections Wordpress plugin
-* @Description: <strong>Meta Collentions&deg;</strong> turns Wordpress into a collection management system. Create Metadata Schema's with easy and intuitively create a user interface for the backend to manage you collection. Besides a professional Collection management tool Meta Collections&deg; can be used to create a Google maps marker collection or a recepy database. Meta Collentions&deg; comes with extensive help and developers documentation.
 * @author URI: http://metacollections.statuur.nl/
 * @license http://www.gnu.org/copyleft/gpl.html GNU Public License version 3
 * @Creation date: September 2012
 * @access Public
+* @Description: <strong>Meta Collentions&deg;</strong> turns Wordpress into a collection management system. Create Metadata Schema's with easy and intuitively create a user interface for the backend to manage you collection. Besides a professional Collection management tool Meta Collections&deg; can be used to create a Google maps marker collection or a recepy database. Meta Collentions&deg; comes with extensive help and developers documentation.
+
 * Text Domain: _coll
 */
 
