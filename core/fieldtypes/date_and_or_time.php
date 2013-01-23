ba64ec8d78ddf72c1ad7ec74ada28d5a385a81d6
bastiaaaan <bastiaaaan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-23 13:49:15 +0000

    updating to 1.0.3
    git-svn-id: https://plugins.svn.wordpress.org/meta-collections/tags/1.0.3@657382 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ core/fieldtypes/date_and_or_time.php
@@ -0,0 +1,200 @@
+<?php
+/**
+  * Handles all functions regarding the field type date and or time (jquery mobiscroller)
+  *
+  *
+  * @author  Bastiaan Blaauw <statuur@gmail.com>
+  *
+  * @author URI: http://metacollections.statuur.nl/
+  * @see http://mobiscroll.com/
+  * @license http://www.gnu.org/copyleft/gpl.html GNU Public License version 3
+  * @access Public
+  * @package  Collections Wordpress plugin
+  */
+
+class Date_and_or_time extends Basics{
+	var $options;
+	
+	function __construct($meta=null){
+		
+	parent::init();
+	$this->Field 		= new Field();	
+	$this->fieldname 	= __("Date and / or Time", "_coll");
+
+	}
+	
+	
+/**
+    * Shows the specific fieldtype in UI::edituserinterface, post(-new).php or edit.php. 
+    * @access public
+    * @param object $post the post info
+    * @param array $element info about the metadata field
+    */	
+	
+function showfield($post=null, $element=null){
+					
+			$element 	= ($element[id]!="") ? $element[args]: $element;
+			$name	 	= $this->postmetaprefix.$element[ID];
+
+			$values	 	= get_post_meta($post->ID, $name, true); 
+			$values	 	= ($values=="" && $element[default_value]!="") ? $element[default_value] : $values;
+			$values	 	= (!is_array($values)) ? array($values) : $values;
+
+			$required 	= ($element[required]==1) ? "class=\"required date_time\" " : "class=\"date_time\" ";
+		
+
+			if($element[required]==1){
+			$_SESSION[required][$element[ID]] = $element[required_err]	;
+			}
+	
+			$html = "";
+
+			foreach ($values as $value){
+			$html.="<div class=\"metafield-value\">
+			<label for=\"{$element[ID]}\">{$element[label]}:</label><br/>
+			<input type=\"text\" {$required} name=\"{$name}[]\" value=\"{$value}\"/> 
+			<a class=\"delete_metavalue\" title=\"".__("delete this", "_coll")." {$element[label]}\" href=\"#\" onclick=\"remove_value_instance(this);return false;\">&nbsp;</a>
+			</div>";
+			}
+		
+			$this->Field->metafieldBox($html, $element);
+			//minDate: new Date(now.getFullYear(), now.getMonth(), now.getDate()),var now = new Date();
+			echo"<script>
+			date_time_preset = '{$element[preset]}';
+			jQuery(document).ready(function () {
+			
+			jQuery('.date_time').scroller({
+			preset: date_time_preset,
+			
+			display: 'modal',
+			mode: 'mixed'
+			
+			});
+
+			});
+			</script>";
+			
+			}
+
+
+/**
+    * Shows the specific form for the fieldtype iwith all the options related to that field. 
+    * @access public
+    */	
+function fieldOptions($element){
+//$element (){
+	
+echo"<table class=\"widefat metadata\" cellpadding=\"10\">";
+$statusc = ($element[status]==1)? "checked":"";
+	
+	
+	$this->Field->getID($element);
+	
+	echo"
+	
+	
+	<tr>
+	<td>".__("Type").":</td>
+	<td>";
+	
+	 $this->Field->getfieldSelect($element);
+	
+	echo"</td>
+	</tr>
+	<tr>
+	<td style=\"width:25%\">".__("Status").":</td>
+	<td><input type=\"checkbox\" {$statusc} name=\"status\" value=\"1\"/></td>
+	</tr>
+	
+	<tr>
+	<td style=\"width:25%\">".__("Label").":</td>
+	<td><input type=\"text\" name=\"label\" value=\"{$element[label]}\"/></td>
+	</tr>
+	
+	<tr>
+	<td>".__("Description").":</td>
+	<td><textarea name=\"description\" rows=\"3\" cols=\"60\">{$element[description]}</textarea></td>
+	</tr>
+
+
+	<tr>
+	<td>".__("Required", "_coll").":</td>
+	<td>";
+	
+	$r_checked_yes	= ($element[required]==1)? "checked": "";
+	$r_checked_no	= ($element[required]==0)? "checked": "";
+	echo"<ul class=\"radio_list radio vertical\">
+                <li><label><input type=\"radio\" value=\"1\" name=\"required\" {$r_checked_yes}> ".__("Yes")."</label></li>
+                <li><label><input type=\"radio\" value=\"0\" name=\"required\" {$r_checked_no}> ".__("No")."</label></li>
+                </ul>
+	
+	</td>
+	</tr>
+	
+	<tr>
+	<td>".__("Required Errormessage", "_coll").":</td>
+	<td><input type=\"text\" name=\"required_err\" value=\"{$element[required_err]}\"/>
+	
+	</td>
+	</tr>";
+	
+	
+	$date_c		= ($element[preset]=="date")? "checked": "";
+	$time_c		= ($element[preset]=="time")? "checked": "";
+	$datetime_c	= ($element[preset]=="datetime")? "checked": "";	
+	echo"<tr>
+	<td>".__("Date type", "_coll").":</td>
+	<td>
+	<input type=\"radio\" name=\"preset\" value=\"date\" {$date_c}/> Date<br/>
+	<input type=\"radio\" name=\"preset\" value=\"time\" {$time_c}/> Time<br/>
+	<input type=\"radio\" name=\"preset\" value=\"datetime\" {$datetime_c}/> Datetime<br/>
+	
+	
+	</td>
+	</tr>
+	
+<tr>
+	<td valign=\"top\">".__("Allow multiple values / instances of this element", "_coll").":</td>
+	<td valign=\"top\">";
+	
+	$m_checked_yes	= ($element[multiple]==1)? "checked": "";
+	$m_checked_no	= ($element[multiple]==0)? "checked": "";
+
+	
+	echo"<ul class=\"radio_list radio vertical\">
+                <li><label><input type=\"radio\" value=\"1\" name=\"multiple\" {$m_checked_yes}> ".__("Yes")."</label></li>
+                <li><label><input type=\"radio\" value=\"0\" name=\"multiple\" {$m_checked_no}> ".__("No")."</label></li>
+                </ul>
+	
+	</td>
+	</tr>	
+
+<tr>
+	<td valign=\"top\">".__("Show this field in Collection overview<br/>(this field has to be dragged in user interface before showing up)", "_coll").":</td>
+	<td valign=\"top\">";
+	
+	$s_checked_yes	= ($element[overview]==1)? "checked": "";
+	$s_checked_no	= ($element[overview]==0)? "checked": "";
+
+	
+	echo"<ul class=\"radio_list radio vertical\">
+                <li><label><input type=\"radio\" value=\"1\" name=\"overview\" {$s_checked_yes}> ".__("Yes")."</label></li>
+                <li><label><input type=\"radio\" value=\"0\" name=\"overview\" {$s_checked_no}> ".__("No")."</label></li>
+                </ul>
+	
+	</td>
+	</tr>
+	
+	<tr>
+	<td colspan=\"2\" style=\"padding:10px\">
+	<a href=\"#\" onclick=\"save_metafield('{$element[ID]}', '{$element[cpt]}', '".__("Field Options Saved")."...');\" class=\"button-primary\" id=\"savemetafield\">".__("Save")."</a></td>
+	</tr>
+	
+	
+	</table>";
+
+//Cancel <a href=\"#\" onclick=\"\" class=\"button\">".__("Cancel")."</a>
+}
+
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,120 @@
+=== Meta Collections ===
+Contributors: bastiaaaan
+
+Tags: collection, collection management, post type, custom taxonomy, custom post type, register_post_type, custom fields, custom, taxonomy,edit screen
+Requires at least: 3.0
+Tested up to: 3.4.1
+Version: 1.0.2
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl.html
+
+Turns Wordpress into a Collection Manager. Customize edit screen by creation metadata schema's for custom post types, taxomonies and fields
+
+== Description ==
+<h4>Meta Collections</h4>
+Meta Collections is developed to turn Wordpress into a Collection manager.
+Beside 'Posts' and 'Pages' you can add your own post type (a Collection e.g. 'Films'). 
+For that particular post type you can a you own Metadata schema. So for example, if you want a Dublin Core schema you can.
+That schema can be a mixture of system and custom fields or a completely customized metadataschema.
+On top of that you can intuitively compose the user interface for the post type by dragging and dropping metafields in the right places using neat metaboxes.
+ 
+Fields can be validated and can consist of multiple instances (e.g. more dan one ingredient field).
+You can also add metafields in the overview table. 
+
+<h4>Fields types</h4>
+
+* Text
+* Textarea
+* Wysiwyg (Wordpress' own Editor)
+* ColorPicker (jQuery UI Colorpicker with hsb, rgb, lab, cmyk, hex colortype and transparancy support)   
+* Date (jQuery UI datepicker)
+* Datetime (jQuery Mobiscroll)
+* Image 
+* Georeference (Using google maps API for displaying a map and (reverse) geocode)
+* True / False (checkbox with text comment)
+* Taxonomy (adds a 'Wordpress own' taxonomy to your post type)
+* Select (for selecting one or multiple values in a dropdownmenu)
+
+Future (shortly available)
+* Youtube and Vimeo
+* File
+* multiple checkboxes
+* radio
+* ask/request!
+
+
+<h4>Shortcode</h4>
+Shorcodes is still quity simple since this plugin is brand new (september 2012)
+you can add the follow shorcode in your posts description in order to use the fields
+<code> [collections metafield="identifier" instance="all" seperator=" - "]</code>
+
+The shorcode analyzed:
+1. 'collections' to identify the shortcode for the collections plugin. alwya begin the shorcode with
+1. 'metafield= metafieldID' you can look a metafieldID up by editing a metadataset and then a metadatafield. 
+1. 'instance = number'  to enable use of multiple values in a metafield. use 0 for the value for instance 0 or 1 for instance 1. Default if variable is not used is 0.
+Use 'all' to return all the values.
+1. 'seperator = 'number' if you use 'All'for 'instance' then you can define a 'seperator' which seperate the values.
+
+<h4>Localized interface</h4>
+The plugin is published in a language supporing two languages, English and Dutch. Feel free to translate it to another language.
+
+
+== Installation ==
+1. Upload the plugin folder` to the `/wp-content/plugins/` directory 
+1. ..or just download it trough the wordpress new plugin interface searching for 'meta collections'
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Create a Collection
+1. Create a Metadata Schema
+1. Compose the Collection's User Interface
+1. Start managing your collection
+1. Build your collection details into your template or use shortcode
+
+
+== Frequently Asked Questions ==
+
+= I added a metafield but it doesn't show up in the post type edit screen =
+
+You first have to drag it into the user interface. Go to Collections, click on 'edit user interface' and drag the field in a place you wan't it  
+
+
+== Screenshots ==
+
+1. Edit screen Collections
+2. Metadataschema
+3. Edit Screen Collections
+4. Compose your user Interface
+5. Start managing your collection
+
+== Changelog ==
+
+= 1.0 =
+* This is the first release.
+
+= 1.0.1 = 
+* Some issues fixed for php 5.2 compatibility
+* fixed some minor bugs
+
+= 1.0.2 = 
+* Fixed path errors for metadatafields
+* Number of fatal errors fixes
+* Bug fixed in save_metabox() name metabox saved properly now
+* Added functionality to order fields in the table overview
+* Tags and Category support for Collections
+* Empty container 'class' caused the drop field in 'Side' to be to short
+* Changes all old names 'collections' to 'metacollections' 
+
+== Upgrade Notice ==
+= 1.0 =
+* This is the first release so no notices.
+
+= 1.0.1 = 
+* Some issues fixed for php 5.2 compatibility
+
+= 1.0.2 = 
+* Fixed path errors for metadatafields
+* Number of fatal errors fixes
+* Bug fixed in save_metabox() name metabox saved properly now
+* Added functionality to order fields in the table overview
+* Tags and Category support for Collections
+* Empty container 'class' caused the drop field in 'Side' to be to short
+* Changes all old names 'collections' to 'metacollections' 
