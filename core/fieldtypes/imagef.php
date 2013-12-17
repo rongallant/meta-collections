43be4b4c3650d7a0fb4297eaa3c38e8079a55a1e
bastiaaaan <bastiaaaan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-17 14:33:57 +0000

    copying new tag files back to 1.0.3
    git-svn-id: https://plugins.svn.wordpress.org/meta-collections/tags/1.0.3@823961 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ core/fieldtypes/imagef.php
@@ -0,0 +1,220 @@
+<?php
+ /**
+  * Handles all functions regarding the field type image
+  *
+  *
+  * @author  Bastiaan Blaauw <statuur@gmail.com>
+  * @see http://www.w3schools.com/tags/tag_input.asp
+  * @author URI: http://metacollections.statuur.nl/
+  * @license http://www.gnu.org/copyleft/gpl.html GNU Public License version 3
+  * @access Public
+  * @package  Collections Wordpress plugin
+  * @todo this type of field type classes probably don't need a constructor function
+  */
+class Imagef extends Basics{
+	
+	function __construct($meta=null){
+	
+	parent::init();
+	$this->Field 		= new Field();
+	$this->fieldname 	= __("Image field", "_coll");
+}
+	
+	
+/**
+    * Shows the specific fieldtype in UI::edituserinterface, post(-new).php or edit.php. 
+    * @access public
+    * @param object $post the post info
+    * @param array $element info about the metadata field
+    */	
+public function showfield($post=null, $element=null){
+		global $post;		
+		
+			$element 	= ($element[id]!="") ? $element[args]: $element;
+			$name	 	= $this->postmetaprefix.$element[ID];
+
+			$values	 	= get_post_meta($post->ID, $name, true); 
+			$values	 	= ($values=="" && $element[default_value]!="") ? $element[default_value] : $values;
+			$values	 	= (!is_array($values)) ? array($values) : $values;
+
+			$required 	= ($element[required]==1) ? "class=\"required\" " : "";
+			$max_length = ($element[max_length]!="") ? " maxlength=\"{$element[max_length]}\"" :"";
+			$length 	= ($element[max_length]!="") ? " size=\"".($element[max_length]+2)."\"" :"20";
+		
+
+			if($element[required]==1){
+			$_SESSION[required][$element[ID]] = $element[required_err]	;
+			}
+			
+		
+			
+			$html = "";
+
+			foreach ($values as $value){//[]
+			$html.="<div class=\"metafield-value\">
+			<label for=\"{$element[ID]}\">{$element[label]}:</label><br/>
+			<input type=\"text\" {$required} size=\"90\" {$length} name=\"{$name}[]\" value=\"{$value}\"/> 
+			<a class=\"button-secondary upload-image-button\" onclick=\"tb_show('Test', 'media-upload.php?post_id={$post->ID}&type=image&wpcf-fields-media-insert=1&TB_iframe=true');return false;\">".__("get / upload image","_coll")."</a>
+		 
+			<a class=\"delete_metavalue\" title=\"".__("delete this", "_coll")." {$element[label]}\" href=\"#\" onclick=\"remove_value_instance(this);return false;\">&nbsp;</a>
+			</div>";
+			}
+		
+			$this->Field->metafieldBox($html, $element);
+			
+			
+			echo"<script>
+			
+			jQuery('.upload-image-button').click(function() {
+    tb_show('', 'media-upload.php?TB_iframe=true&post_id={$post->ID}');
+    
+    window.send_to_editor = function(html) {
+        url = jQuery(html).attr('href');
+        jQuery('input[name$=\"{$name}[]\"]').val(url);
+        tb_remove();
+    };
+    return false;
+});
+
+</script>
+";
+			}
+
+/**
+    * Shows the specific form for the fieldtype iwith all the options related to that field. 
+    * @access public
+    */	
+public function fieldOptions($element){
+
+echo"<table class=\"widefat metadata\" cellpadding=\"10\">";
+$statusc = ($element[status]==1)? "checked":"";
+	
+	$this->Field->getID($element);
+	
+	echo"
+	
+	<tr>
+	<td>".__("Type").":</td>
+	<td>";
+	
+	 $this->Field->getfieldSelect($element);
+	
+	echo"</td>
+	</tr>
+
+	<tr>
+	<td style=\"width:25%\">".__("Status").":</td>
+	<td><input type=\"checkbox\" {$statusc} name=\"status\" value=\"1\"/></td>
+	</tr>
+	
+	<tr>
+	<td style=\"width:25%\">".__("Label").": *</td>
+	<td><input type=\"text\" name=\"label\" class=\"required\" value=\"{$element[label]}\"/></td>
+	</tr>
+	
+	<tr>
+	<td>".__("Description").":</td>
+	<td><textarea name=\"description\" rows=\"3\" cols=\"60\">{$element[description]}</textarea></td>
+	</tr>
+
+	
+	<tr>
+	<td>".__("Required", "_coll").":</td>
+	<td>";
+	
+	$r_checked_yes	= ($element[required]==1)? "checked": "";
+	$r_checked_no	= ($element[required]==0)? "checked": "";
+	echo"<ul class=\"radio_list radio vertical\">
+                <li><label><input type=\"radio\" value=\"1\" name=\"required\" {$r_checked_yes}> ".__("Yes")."</label></li>
+                <li><label><input type=\"radio\" value=\"0\" name=\"required\" {$r_checked_no}> ".__("No")."</label></li>
+                </ul>
+	
+	</td>
+	</tr>
+	
+	<tr>
+	<td>".__("Required Errormessage", "_coll").":</td>
+	<td><input type=\"text\" name=\"required_err\" value=\"{$element[required_err]}\"/>
+	
+	</td>
+	</tr>
+	
+	<tr>
+	<td>".__("Default Value", "_coll").":</td>
+	<td><input type=\"text\" name=\"default_value\" value=\"{$element[default_value]}\"/>
+	
+	</td>
+	</tr>
+	
+	<tr>
+	<td>".__("Max Length", "_coll").":<br/>".__("maximum character length of the field value", "_coll")."</td>
+	<td><select name=\"max_length\">";
+	//<input type=\"text\" name=\"max_length\" value=\"{$element[max_length]}\"/>
+	$selector = ($element[max_length]=="")? 20 : $element[max_length];
+	for ($i=1; $i<100; $i++){
+	$selected = ($i==$selector)? "selected":"";	
+	echo"<option value=\"{$i}\" {$selected}>$i</option>";
+	}
+	echo"</select>
+	
+	
+	
+	</td>
+	</tr>	
+	
+	<tr>
+	<td valign=\"top\">".__("Allow multiple values / instances of this element", "_coll").":</td>
+	<td valign=\"top\">";
+	
+	$m_checked_yes	= ($element[multiple]==1)? "checked": "";
+	$m_checked_no	= ($element[multiple]==0)? "checked": "";
+
+	
+	echo"<ul class=\"radio_list radio vertical\">
+                <li><label><input type=\"radio\" value=\"1\" name=\"multiple\" {$m_checked_yes}> ".__("Yes")."</label></li>
+                <li><label><input type=\"radio\" value=\"0\" name=\"multiple\" {$m_checked_no}> ".__("No")."</label></li>
+                </ul>
+	
+	</td>
+	</tr>	
+
+<tr>
+	<td valign=\"top\">".__("Show this field in Collection overview<br/>(this field has to be dragged in user interface before showing up)", "_coll").":</td>
+	<td valign=\"top\">";
+	
+	$s_checked_yes	= ($element[overview]==1)? "checked": "";
+	$s_checked_no	= ($element[overview]==0)? "checked": "";
+
+	$formID = "#edit_options_{$element[ID]}_{$element[cpt]}";
+	echo"<ul class=\"radio_list radio vertical\">
+                <li><label><input type=\"radio\" value=\"1\" name=\"overview\" {$s_checked_yes}> ".__("Yes")."</label></li>
+                <li><label><input type=\"radio\" value=\"0\" name=\"overview\" {$s_checked_no}> ".__("No")."</label></li>
+                </ul>
+	
+	</td>
+	</tr>
+	
+	<tr>
+	<td colspan=\"2\" style=\"padding:10px\">
+	<a href=\"#\" onclick=\"
+	if(jQuery('{$formID}').validate().form()){
+	save_metafield('{$element[ID]}', '{$element[cpt]}', '".__("Field Options Saved")."...');
+	}return false;
+	\" class=\"button-primary\" id=\"savemetafield\">".__("Save")."</a>	</td>
+	</tr>
+	
+	
+	</table>";
+		
+	echo"<script>
+	 jQuery(document).ready(function(){
+	jQuery('{$formID}').validate();
+   });
+   
+   </script>";
+
+//Cancel <a href=\"#\" onclick=\"\" class=\"button\">".__("Cancel")."</a>
+}
+
+}
+?>
